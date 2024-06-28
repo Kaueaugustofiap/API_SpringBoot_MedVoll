bb9a315360d1af9bfd2e0eca7e20abe750d3a946
Kaue <kaueaugusto2002@hotmail.com>
2024-06-28 16:20:36 -0300

    Atualizando MedicoController
    Adicionando os métodos de atualizar de excluir

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,7 +27,29 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    /*Método que exlcui o registro do banco de dados
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void exlcuir(@PathVariable Long id){
+        repository.deleteById(id);
+    }*/
+
+    //Método que faz a exlcusão lógica do banco de dados, ou seja, deixa inativo o registro
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void exlcuir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 
 }
