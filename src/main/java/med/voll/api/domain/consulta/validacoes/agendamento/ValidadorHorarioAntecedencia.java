d5c279ee1845bc43bffd3ddec5f7a7056a252a37
Kaue <kaueaugusto2002@hotmail.com>
2024-07-24 14:57:46 -0300

    Criada a classe ValidadorAgendamentoDeConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulsta;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsulta{
+
+    public void validar(DadosAgendamentoConsulsta dados){
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com atencedência mínima de 30 minutos");
+        }
+
+    }
+}
