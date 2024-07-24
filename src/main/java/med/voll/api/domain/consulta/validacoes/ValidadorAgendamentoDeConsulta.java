f3e93053bd75b1133acf2ad43be7b209c750c7ca
Kaue <kaueaugusto2002@hotmail.com>
2024-07-24 13:59:55 -0300

    Criando interface ValidadorAgendamentoDeConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulsta;
+
+public interface ValidadorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulsta dados);
+
+}
