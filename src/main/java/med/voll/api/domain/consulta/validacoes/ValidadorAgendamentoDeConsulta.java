d7acc988db601780a5ba77a9e5d34ea6f278734a
Kaue <kaueaugusto2002@hotmail.com>
2024-07-24 14:40:45 -0300

    Deleção de código desnecessários

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.domain.consulta.validacoes;
-
-import med.voll.api.domain.consulta.DadosAgendamentoConsulsta;
-
-public interface ValidadorAgendamentoDeConsulta {
-
-    void validar(DadosAgendamentoConsulsta dados);
-
-}
