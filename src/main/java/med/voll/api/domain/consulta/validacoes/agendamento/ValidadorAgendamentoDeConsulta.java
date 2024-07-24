81f50bcc5441ce55ea0983c04f2ca3861cf71086
Kaue <kaueaugusto2002@hotmail.com>
2024-07-24 14:41:08 -0300

    Criada a interface ValidadorAgendamentoConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorAgendamentoDeConsulta.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.consulta.validacoes.agendamento;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulsta;
+
+public interface ValidadorAgendamentoDeConsulta {
+
+    void validar(DadosAgendamentoConsulsta dados);
+
+}
