64e7bb7ea9346e627503d4bf1825df0a7b816d68
Kaue <kaueaugusto2002@hotmail.com>
2024-07-24 14:58:02 -0300

    Criada a classe ValidadorHorarioAntecedenciaCancelamentoConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecendeciaCancelamentoConsulta.java
@@ -0,0 +1,27 @@
+package med.voll.api.domain.consulta.validacoes.cancelamento;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioAntecendeciaCancelamentoConsulta implements ValidadorCancelamentoDeConsulta {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void validar(DadosCancelamentoConsulta dados){
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        var agora = LocalDateTime.now();
+        var diferencaEmHoras = Duration.between(agora, consulta.getData()).toHours();
+
+        if(diferencaEmHoras < 24){
+            throw new ValidacaoException("Uma consulta somente poderá ser cancelada com antecedência mínima de 24 horas.");
+        }
+    }
+}
